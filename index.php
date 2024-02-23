c3ec9fffaa3b1d119febf114e538d96e4826d0ba
y0unique <matsan00123@gmail.com>
2024-02-23 15:50:35 +0800

    update for images for front web

--- index.php
+++ index.php
@@ -5,6 +5,16 @@
     <head>
         <meta charset="utf-8" />
         <title>Deped Manila</title>
+        <script>
+            document.addEventListener('contextmenu', function (e) {
+                e.preventDefault();
+            });
+            document.addEventListener('keydown', function (e) {
+                if (e.code === 'KeyU' && (e.ctrlKey || e.metaKey)) {
+                    e.preventDefault();
+                }
+            });
+        </script>
         <meta name="viewport"    content="width=device-width, initial-scale=1.0">
         <meta name="description" content="Department of Education Manila" />
         <meta name="keywords"    content="DepEd, Manila, Schools Division, Division of City Schools, Website of DepEd Manila, Official Website" />
@@ -25,12 +35,15 @@
 
     <body>
         <!-- Navbar Start -->
-        <nav class="navbar navbar-expand-lg fixed-top navbar-custom navbar-light sticky">
+        <nav class="navbar navbar-expand-lg fixed-top navbar-custom navbar-light nav-sticky">
     		<div class="container">
-			    <a class="navbar-brand" href="index">deped manila</a>
-			    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
-			        <span class="mdi mdi-menu"></span>
-			    </button><!--end button-->
+                <a href="index"><img src="home/images/logo/DepEd_logo.png"
+                    style="height: 50px; width:50px; margin-right:5px;"></a>
+                <a class="navbar-brand " href="" target="__blank">DEPED MANILA</a>
+			    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse"
+                    aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="mdi mdi-menu"></span>
+                </button><!--end button-->
 
 			    <div class="collapse navbar-collapse" id="navbarCollapse">
 			        <ul class="navbar-nav ml-auto">
@@ -906,25 +919,6 @@
         </section><!--end section-->
         <!-- Pricing End -->
 
-        <!-- Partners Start -->
-        <section class="section-two bg-light">
-            <div class="container">
-                <div class="row">
-                    <div class="col-12">
-                        <div class="slider autoplay">
-                            <div><img src="home/images/testi/partners1.png" class="img-fluid mx-auto d-block" alt="partners"></div>
-                            <div><img src="home/images/testi/partners2.png" class="img-fluid mx-auto d-block" alt="partners"></div>
-                            <div><img src="home/images/testi/partners3.png" class="img-fluid mx-auto d-block" alt="partners"></div>
-                            <div><img src="home/images/testi/partners4.png" class="img-fluid mx-auto d-block" alt="partners"></div>
-                            <div><img src="home/images/testi/partners5.png" class="img-fluid mx-auto d-block" alt="partners"></div>
-                            <div><img src="home/images/testi/partners6.png" class="img-fluid mx-auto d-block" alt="partners"></div>
-                        </div>
-                    </div><!--end col-->
-                </div><!--end row-->
-            </div><!--end container-->
-        </section><!--end section-->
-        <!-- Partners End -->
-
         <!-- Blog Start -->
         <section class="section" id="news">
             <div class="container">
@@ -1127,53 +1121,76 @@
         </section><!--end section-->
         <!-- Contact End -->
 
+        <!-- Seals Start -->
+        <section class="section-two bg-light">
+            <div class="container">
+                <div class="row">
+                    <div class="col-12">
+                        <div class="slider autoplay">
+                        <div><img src="home/images/testi/freedomOfInfo.png" class="img-fluid mx-auto d-block" alt="partners"></div>
+                            <div><img src="home/images/testi/Department_of_Education.png" class="img-fluid mx-auto d-block" alt="partners"></div>
+                            <div><img src="home/images/testi/TransSeal.png" class="img-fluid mx-auto d-block" alt="partners"></div>
+                            <div><img src="home/images/testi/freedomOfInfo.png" class="img-fluid mx-auto d-block" alt="partners"></div>
+                            <div><img src="home/images/testi/Department_of_Education.png" class="img-fluid mx-auto d-block" alt="partners"></div>
+                            <div><img src="home/images/testi/TransSeal.png" class="img-fluid mx-auto d-block" alt="partners"></div>
+                        </div>
+                    </div><!--end col-->
+                </div><!--end row-->
+            </div><!--end container-->
+        </section><!--end section-->
+        <!-- Seals End -->
+
         <!-- Footer Start -->
         <footer class="footer py-5 bg-dark">
             <div class="container">
                 <div class="row">
-                    <div class="col-lg-4 col-md-12">
-                        <a class="footer-logo" href="javscript:void(0)">Orayo</a>
-                        <p class="text-foot mt-3">Launch your campaign and benefit from our expertise on designing</p>
-                        <ul class="list-unstyled social-icon social mb-0 mt-4">
-                            <li class="list-inline-item"><a href="javascript:void(0)"><i class="mdi mdi-facebook" title="Facebook"></i></a></li>
-                            <li class="list-inline-item"><a href="javascript:void(0)"><i class="mdi mdi-linkedin" title="Linkedin"></i></a></li>
-                            <li class="list-inline-item"><a href="javascript:void(0)"><i class="mdi mdi-youtube" title="Youtube"></i></a></li>
-                            <li class="list-inline-item"><a href="javascript:void(0)"><i class="mdi mdi-instagram" title="Instagram"></i></a></li>
-                            <li class="list-inline-item"><a href="javascript:void(0)"><i class="mdi mdi-twitter" title="Twitter"></i></a></li>
-                        </ul><!--end icon-->
-                    </div><!--end col-->
-
-                    <div class="col-lg-2 col-md-3 mt-4 pt-2 mt-lg-0 pt-lg-0">
-                        <h4 class="text-light text-uppercase footer-head">Links</h4>
+                    <div class="col-lg-4 col-md-12 mt-4 pt-2 mt-lg-0 pt-lg-0">
+                        <a class="footer-logo back-to-top" href="#">DepEd Manila</a>
                         <ul class="list-unstyled footer-list mt-4 mb-0">
-                            <li><a href="javascript:void(0)" class="text-foot"><i class="mdi mdi-chevron-right mr-2"></i> Aboutus</a></li>
-                            <li class="mt-2"><a href="javascript:void(0)" class="text-foot"><i class="mdi mdi-chevron-right mr-2"></i> Services</a></li>
-                            <li class="mt-2"><a href="javascript:void(0)" class="text-foot"><i class="mdi mdi-chevron-right mr-2"></i> Team</a></li>
-                            <li class="mt-2"><a href="javascript:void(0)" class="text-foot"><i class="mdi mdi-chevron-right mr-2"></i> Pricing</a></li>
+                            <li class="mt-2"><a href="javascript:void(0)" class="text-foot"><i class="mdi mdi-map-marker"></i> Address: Manila Education Center Arroceros Forest Park Antonio J. Villegas St. Ermita, Manila</a></li>
+                            <li class="mt-2"><a href="javascript:void(0)" class="text-foot"><i class="mdi mdi-phone"></i> Contact Details: 85275216 • 85275009 • 87351029</a></li>
+                            <li class="mt-2"><a href="javascript:void(0)" class="text-foot"><i class="mdi mdi-email"></i> Email Address: sdo.manila@deped.gov.ph</a></li>
+                        </ul>
+                        
+                        <ul class="list-unstyled social-icon social mb-0 mt-4">
+                            <li class="list-inline-item"><a href="https://www.facebook.com/DepEdManilaOfficial/" target="__blank"><i class="mdi mdi-facebook" title="Facebook"></i></a></li>
+                            <li class="list-inline-item"><a href="https://www.youtube.com/@depedmanilaYT" target="__blank"><i class="mdi mdi-youtube" title="Youtube"></i></a></li>
                         </ul>
                     </div>
+                    <div class="col-lg-4 col-md-12 mt-4 pt-2 mt-lg-0 pt-lg-0">
+                        <h4 class="text-light text-uppercase footer-head"><img src="home/images/testi/footlogo.png" alt="logo" style="height: 20px; width:20px;"> REPUBLIC OF THE PHILIPPINES</h4>
+                            <ul class="list-unstyled footer-list mt-4 mb-0">
 
-                    <div class="col-lg-3 col-md-5 mt-4 pt-2 mt-lg-0 pt-lg-0">
-                        <h4 class="text-light text-uppercase footer-head">Tags</h4>
-                        <div class="tagcloud mt-4">
-                            <a href="javascript:void(0)">Agency</a>
-                            <a href="javascript:void(0)">Responsive</a>
-                            <a href="javascript:void(0)">Slider</a>
-                            <a href="javascript:void(0)">Services</a>
-                            <a href="javascript:void(0)">Price</a>
-                            <a href="javascript:void(0)">News</a>
-                            <a href="javascript:void(0)">Hero</a>
-                            <a href="javascript:void(0)">Reviews</a>
-                            <a href="javascript:void(0)">CTA</a>
-                        </div>
+                                <ul class="list-unstyled text-foot mt-4 mb-0">
+                                    <li>All content is in the public domain unless otherwise state</li>
+                                </ul>
+
+                                <ul class="list-unstyled text-foot mt-4 mb-0">
+                                    <h4 class="text-light text-uppercase footer-head">ABOUT GOVPH</h4>
+                                    <li>Learn more about the Philippine government, its structure, how government works and the people behind it.</li>
+                                </ul>
+
+                                <li class="mt-2"><a href="https://www.gov.ph/" class="text-foot" target="__blank"><i class="mdi mdi-chevron-right mr-2"></i> GOV.PH</a></li>
+                                <li class="mt-2"><a href="https://www.officialgazette.gov.ph/" class="text-foot" target="__blank"><i class="mdi mdi-chevron-right mr-2"></i> Official Gazette</a></li>
+                                <li class="mt-2"><a href="https://data.gov.ph/index/home" class="text-foot" target="__blank"><i class="mdi mdi-chevron-right mr-2"></i> Open Data Portal</a></li>
+
+                                
+                            </ul>
+
                     </div>
 
-                    <div class="col-lg-3 col-md-4 mt-4 pt-2 mt-lg-0 pt-lg-0">
-                        <h4 class="text-light text-uppercase footer-head">Business Hours</h4>
-                        <ul class="list-unstyled text-foot mt-4 mb-0">
-                            <li>Monday - Friday : 9:00 to 17:00</li>
-                            <li class="mt-2">Saturday : 10:00 to 15:00</li>
-                            <li class="mt-2">Sunday : Day Off (Holiday)</li>
+                    <div class="col-lg-4 col-md-12 mt-4 pt-2 mt-lg-0 pt-lg-0">
+                        <h4 class="text-light text-uppercase footer-head">GOVERNMENT LINKS</h4>
+                        <ul class="list-unstyled footer-list mt-4 mb-0">
+                            <li class="mt-2"><a href="https://president.gov.ph/" class="text-foot" target="__blank"><i class="mdi mdi-chevron-right mr-2"></i> The President</a></li>
+                            <li class="mt-2"><a href="https://op-proper.gov.ph/" class="text-foot" target="__blank"><i class="mdi mdi-chevron-right mr-2"></i> Office of the President</a></li>
+                            <li class="mt-2"><a href="https://ovp.gov.ph/" class="text-foot" target="__blank"><i class="mdi mdi-chevron-right mr-2"></i> Office of the Vice President</a></li>
+                            <li class="mt-2"><a href="http://senate.gov.ph/" class="text-foot" target="__blank"><i class="mdi mdi-chevron-right mr-2"></i> Senate of the Philippines</a></li>
+                            <li class="mt-2"><a href="https://www.congress.gov.ph/" class="text-foot" target="__blank"><i class="mdi mdi-chevron-right mr-2"></i> House of Representatives</a></li>
+                            <li class="mt-2"><a href="http://sc.judiciary.gov.ph/" class="text-foot" target="__blank"><i class="mdi mdi-chevron-right mr-2"></i> Supreme Court</a></li>
+                            <li class="mt-2"><a href="http://ca.judiciary.gov.ph/" class="text-foot" target="__blank"><i class="mdi mdi-chevron-right mr-2"></i> Court of Appeals</a></li>
+                            <li class="mt-2"><a href="https://sb.judiciary.gov.ph/" class="text-foot" target="__blank"><i class="mdi mdi-chevron-right mr-2"></i> Sandiganbayan</a></li>
+                            
                         </ul>
                     </div>
                 </div><!--end row-->
@@ -1184,7 +1201,7 @@
                 <div class="row align-items-center">
                     <div class="col-sm-8">
                         <div class="text-sm-left">
-                            <p class="mb-0">2021 © Orayo. Design by Themesdesign</p>
+                            <p class="mb-0">© <?php echo date("Y"); ?> DepEd Manila</p>
                         </div>
                     </div><!--end col-->
 
@@ -1201,7 +1218,7 @@
         <!-- Footer End -->
 
         <!-- javascript -->
-        <script src="home/js/jquery-3.5.1.min.js"></script>
+        <script src="home/js/jquery-3.7.1.min.js"></script>
         <script src="home/js/bootstrap.bundle.min.js"></script>
         <script src="home/js/jquery.easing.min.js"></script>
         <script src="home/js/scrollspy.min.js"></script>
