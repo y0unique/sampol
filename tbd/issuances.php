1a47ffec1c040fafc428198c84aa88d9ed90672e
y0unique <102183644+y0unique@users.noreply.github.com>
2024-04-01 05:49:58 +0800

    Create issuances.php

---
+++ tbd/issuances.php
@@ -0,0 +1,176 @@
+<div class="row mt-4">
+                    <div class="col-12">
+                        <ul class="nav nav-pills nav-justified" id="pills-tab" role="tablist">
+                            
+                            <li class="nav-item mb-4 pt-2">
+                                <a class="nav-link active" id="past-2019-tab" data-toggle="pill" href="#past-2019" role="tab" aria-controls="past-2019" aria-selected="false">
+                                    <div class="capabilities text-center rounded pt-2 pb-2">
+                                        <div class="icon bg-custom mb-3">
+                                            <i class="mdi mdi-file-multiple text-white"></i>
+                                        </div>
+                                        <h4 class="title font-weight-normal mb-0">2019</h4>
+                                    </div>
+                                </a><!--end nav link-->
+                            </li><!--end nav item-->
+                            
+                            <li class="nav-item mb-4 pt-2">
+                                <a class="nav-link" id="past-2020-tab" data-toggle="pill" href="#past-2020" role="tab" aria-controls="past-2020" aria-selected="false">
+                                    <div class="capabilities text-center rounded pt-2 pb-2">
+                                        <div class="icon bg-custom mb-3">
+                                            <i class="mdi mdi-file-multiple text-white"></i>
+                                        </div>
+                                        <h4 class="title font-weight-normal mb-0">2020</h4>
+                                    </div>
+                                </a><!--end nav link-->
+                            </li><!--end nav item-->
+                            
+                            <li class="nav-item mb-4 pt-2">
+                                <a class="nav-link" id="past-2021-tab" data-toggle="pill" href="#past-2021" role="tab" aria-controls="past-2021" aria-selected="false">
+                                    <div class="capabilities text-center rounded pt-2 pb-2">
+                                        <div class="icon bg-custom mb-3">
+                                            <i class="mdi mdi-file-multiple text-white"></i>
+                                        </div>
+                                        <h4 class="title font-weight-normal mb-0">2021</h4>
+                                    </div>
+                                </a><!--end nav link-->
+                            </li><!--end nav item-->
+                            
+                            <li class="nav-item mb-4 pt-2">
+                                <a class="nav-link" id="past-2022-tab" data-toggle="pill" href="#past-2022" role="tab" aria-controls="past-2022" aria-selected="false">
+                                    <div class="capabilities text-center rounded pt-2 pb-2">
+                                        <div class="icon bg-custom mb-3">
+                                            <i class="mdi mdi-file-multiple text-white"></i>
+                                        </div>
+                                        <h4 class="title font-weight-normal mb-0">2022</h4>
+                                    </div>
+                                </a><!--end nav link-->
+                            </li><!--end nav item-->
+
+                            <li class="nav-item mb-4 pt-2">
+                                <a class="nav-link" id="past-2023-tab" data-toggle="pill" href="#past-2023" role="tab" aria-controls="past-2023" aria-selected="false">
+                                    <div class="capabilities text-center rounded pt-2 pb-2">
+                                        <div class="icon bg-custom mb-3">
+                                            <i class="mdi mdi-file-multiple text-white"></i>
+                                        </div>
+                                        <h4 class="title font-weight-normal mb-0">2023</h4>
+                                    </div>
+                                </a><!--end nav link-->
+                            </li><!--end nav item-->
+
+                        </ul><!--end nav pills-->
+
+                        <div class="tab-content mt-3" id="pills-tabContent">
+
+                            <div class="tab-pane fade show active" id="past-2019" role="tabpanel" aria-labelledby="past-2019-tab">
+                                <div class="capabilities-content border rounded p-4">
+                                    <div class="row align-items-center">
+                                        <div class="col-md-12">
+                                            <h4 class="title">2019</h4>
+                                        	
+                                        	<iframe src="https://docs.google.com/spreadsheets/d/e/2PACX-1vSp9NyIpsd60bSwcSGSR-XT7LTN8If-_Dpis23lBFSn5LJh4sIAPc4JV4bjL0Ue4kPrW65gqqEsuqgM/pubhtml?widget=true&amp;headers=false" style="width:100%; height:1000px; border: 2px solid black;"></iframe>
+                                            
+                                            
+
+                                        </div><!--end col-->
+
+                                    </div><!--end row-->
+                                </div>
+                            </div><!--end teb pane-->
+                            
+                            <div class="tab-pane fade" id="past-2020" role="tabpanel" aria-labelledby="past-2020-tab">
+                                <div class="capabilities-content border rounded p-4">
+                                    <div class="row align-items-center">
+                                        <div class="col-md-12">
+                                            <h4 class="title">2020</h4>
+                                            
+                                            <iframe src="https://docs.google.com/spreadsheets/d/e/2PACX-1vTG3yIf5CgEzqCfaWI78lIfi83s57gBxKp0DBSZV7ye1NVNU3iq_yLoJr2NFZUavzP8jyuvAbbOubUf/pubhtml?widget=true&amp;headers=false" style="width:100%; height:1000px; border: 2px solid black;"></iframe>
+
+                                        </div><!--end col-->
+                                    </div><!--end row-->
+                                </div>
+                            </div><!--end teb pane-->
+
+                            <div class="tab-pane fade" id="past-2021" role="tabpanel" aria-labelledby="past-2021-tab">
+                                <div class="capabilities-content border rounded p-4">
+                                    <div class="row align-items-center">
+                                        <div class="col-md-12">
+                                            <h4 class="title">2021</h4>
+                                            
+                                            <iframe src="https://docs.google.com/spreadsheets/d/e/2PACX-1vQ4AjFZqmXZfhSgG5Q8o0qjuRJk5HcxoYvnXWe7vJ7GrDCagJgImjtBgaPiDGHcaXN7Pa8uHXIVPJRA/pubhtml?widget=true&amp;headers=false" style="width:100%; height:1000px; border: 2px solid black;"></iframe>
+
+                                        </div><!--end col-->
+                                    </div><!--end row-->
+                                </div>
+                            </div><!--end teb pane-->
+
+                            <div class="tab-pane fade" id="past-2022" role="tabpanel" aria-labelledby="past-2022-tab">
+                                <div class="capabilities-content border rounded p-4">
+                                    <div class="row align-items-center">
+                                        <div class="col-md-12">
+                                            <h4 class="title">2022</h4>
+                                            
+                                            <?php
+                                                $issuances = "SELECT * FROM issuancesvw WHERE status = 'active' AND year(date) = 2022 ORDER BY date DESC";
+                                                $issuances1 = mysqli_query($con, $issuances);
+                                                echo '<table id="past-2022-table" class="table table-striped" style="width:100%">';
+                                                echo '<thead>';
+                                                echo '<tr>';
+                                                echo '<th>Date</th> ';
+                                                echo '<th>No.</th>  ';
+                                                echo '<th>Title</th>';
+                                                echo '</tr>';
+                                                echo '</thead>';
+                                                echo '<tbody>';
+                                                while($row = mysqli_fetch_array($issuances1)){
+                                                    echo '<tr>';
+                                                    echo '<td>'.$row['date'].'</td>';
+                                                    echo '<td>'.$row['number'].'</td>';
+                                                    echo '<td><a class="text-primary" href="'.$row['link'].'" target="_blank">'.$row['title'].'</a></td>';
+                                                    echo '</tr>';
+                                                }
+                                                echo '</tbody>';
+                                                echo '</table>'; 
+                                            ?>
+
+                                        </div><!--end col-->
+                                    </div><!--end row-->
+                                </div>
+                            </div><!--end teb pane-->
+                            
+                            <div class="tab-pane fade" id="past-2023" role="tabpanel" aria-labelledby="past-2023-tab">
+                                <div class="capabilities-content border rounded p-4">
+                                    <div class="row align-items-center">
+                                        <div class="col-md-12">
+                                            <h4 class="title">2023</h4>
+                                            
+                                            <?php
+                                                $issuances = "SELECT * FROM issuancesvw WHERE status = 'active' AND year(date) = 2023 ORDER BY date DESC";
+                                                $issuances1 = mysqli_query($con, $issuances);
+                                                echo '<table id="past-2023-table" class="table table-striped" style="width:100%">';
+                                                echo '<thead>';
+                                                echo '<tr>';
+                                                echo '<th>Date</th> ';
+                                                echo '<th>No.</th>  ';
+                                                echo '<th>Title</th>';
+                                                echo '</tr>';
+                                                echo '</thead>';
+                                                echo '<tbody>';
+                                                while($row = mysqli_fetch_array($issuances1)){
+                                                    echo '<tr>';
+                                                    echo '<td>'.$row['date'].'</td>';
+                                                    echo '<td>'.$row['number'].'</td>';
+                                                    echo '<td><a class="text-primary" href="'.$row['link'].'" target="_blank">'.$row['title'].'</a></td>';
+                                                    echo '</tr>';
+                                                }
+                                                echo '</tbody>';
+                                                echo '</table>'; 
+                                            ?>
+
+                                        </div><!--end col-->
+                                    </div><!--end row-->
+                                </div>
+                            </div><!--end teb pane-->
+
+                        </div><!--end tab content-->
+                    </div><!--end col-->
+                </div><!--end row-->
